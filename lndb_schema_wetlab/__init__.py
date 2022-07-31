2fcaa2290f772545880bad0bd905f4c66b79e42d
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-31 12:46:34 +0200

    :bookmark: Release version 0.1.1

--- lndb_schema_wetlab/__init__.py
+++ lndb_schema_wetlab/__init__.py
@@ -15,6 +15,6 @@ This is the complete API reference:
    readout_type
 """
 
-__version__ = "0.1.0"  # denote a pre-release for 0.1.0 with 0.1a1
+__version__ = "0.1.1"  # denote a pre-release for 0.1.0 with 0.1a1
 
 from ._core import biometa, biosample, dobject_biometa, readout_type  # noqa
