ed8c4ab9105eb10df4cba24f34ac28128ef93aa8
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-19 12:21:15 +0200

    :bookmark: Release version 0.2.0

--- lnschema_wetlab/__init__.py
+++ lnschema_wetlab/__init__.py
@@ -17,7 +17,7 @@ This is the complete API reference:
 """
 # This is lnschema-module vvhc.
 _schema_module_id = "vvhc"
-__version__ = "0.1.2"  # denote a pre-release for 0.1.0 with 0.1a1
+__version__ = "0.2.0"  # denote a pre-release for 0.1.0 with 0.1a1
 
 from ._core import (  # noqa
     biometa,
