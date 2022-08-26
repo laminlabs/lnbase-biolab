58b5f48e254396213ab1511def233ac61eac4ab9
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-26 15:02:19 +0200

    :bookmark: Release version 0.3.1

--- lnschema_wetlab/__init__.py
+++ lnschema_wetlab/__init__.py
@@ -20,7 +20,7 @@ This is the complete API reference:
 """
 # This is lnschema-module vvhc.
 _schema_module_id = "vvhc"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 _tables = ["biosample", "techsample"]
 
 from ._core import (  # noqa
