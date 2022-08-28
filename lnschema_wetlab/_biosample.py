109375c09da9d45d13b2167ee207fd1377dc0800
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-28 13:18:31 +0200

    :pencil2: fixed typo

--- lnschema_wetlab/_biosample.py
+++ lnschema_wetlab/_biosample.py
@@ -15,7 +15,7 @@ class biosample(SQLModel, table=True):  # type: ignore
     name: str = Field(default=None, index=True)
     batch: Optional[int] = None
     species_id: int = Field(default=None, foreign_key="species.id")
-    tissue_id: str = Field(default=None, foreign_key="cell_type.id")
+    tissue_id: str = Field(default=None, foreign_key="tissue.id")
     cell_type_id: str = Field(default=None, foreign_key="cell_type.id")
     disease_id: str = Field(default=None, foreign_key="disease.id")
     cell_marker_id: str = Field(default=None, foreign_key="cell_marker.id")
