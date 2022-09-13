85e73179dfd960e256170dc1d2c40e8cf0b0606e
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-09-13 13:21:30 +0200

    :sparkles: Added filepath to techsample

--- lnschema_wetlab/_techsample.py
+++ lnschema_wetlab/_techsample.py
@@ -10,3 +10,5 @@ class techsample(SQLModel, table=True):  # type: ignore
     external_id: str = Field(default=None, index=True)
     name: str = Field(default=None, index=True)
     batch: Optional[str] = None
+    filepath_r1: Optional[str] = None
+    filepath_r2: Optional[str] = None
