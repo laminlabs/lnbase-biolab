9233290b4fd6dffdde09b8d1d0f54ab5244dc677
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-28 22:27:45 +0800

    :bookmark: Release version 0.10.2

--- lnschema_wetlab/__init__.py
+++ lnschema_wetlab/__init__.py
@@ -29,7 +29,7 @@ Development tools:
 _schema_id = "vvhc"
 _name = "wetlab"
 _migration = "bfda12fc80a8"
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 from . import dev, link
 from ._core import (  # noqa
