d54f3edccf319b6b7bdfb3fd61e2fd9b03388ead
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-07 02:31:29 +0100

    🎨 Refactor, turn all classes into base classes, drop `Treatment` from this module (#50)
    * :art: Refactor, make everything base classes, drop treatment
    
    * :green_heart: Fixes
    
    * :green_heart: Fix
    
    * :green_heart: Fix

---
+++ lnschema_wetlab/_actual.py
@@ -0,0 +1,107 @@
+from typing import Optional
+
+from lnschema_core import DObject
+from lnschema_core.dev.sqlmodel import add_relationship_keys, schema_sqlmodel
+from sqlalchemy.orm import relationship
+from sqlmodel import Field, Relationship
+
+from . import _name as schema_name
+from ._core import (
+    BiosampleBase,
+    ExperimentBase,
+    ExperimentTypeBase,
+    ReadoutBase,
+    TechsampleBase,
+)
+from ._link import (
+    BiosampleTechsample,
+    DObjectBiosample,
+    DObjectExperiment,
+    DObjectReadout,
+)
+
+_, prefix, schema_arg = schema_sqlmodel(schema_name)
+
+
+class Experiment(ExperimentBase, table=True):  # type: ignore
+    """Experiments."""
+
+    experiment_type_id: str = Field(
+        default=None, foreign_key="wetlab.experiment_type.id", index=True
+    )
+    dobjects: DObject = Relationship(
+        back_populates="experiments",
+        sa_relationship_kwargs=dict(secondary=DObjectExperiment.__table__),
+    )
+
+
+DObject.experiments = relationship(
+    Experiment, back_populates="dobjects", secondary=DObjectExperiment.__table__
+)
+DObject.__sqlmodel_relationships__["experiments"] = None
+
+
+class ExperimentType(ExperimentTypeBase, table=True):  # type: ignore
+    """Experiment types."""
+
+    __tablename__ = f"{prefix}experiment_type"
+
+
+class Biosample(BiosampleBase, table=True):  # type: ignore
+    """Biological samples that are registered in experiments."""
+
+    batch: Optional[str] = None
+    tissue_id: Optional[str] = Field(
+        default=None, foreign_key="bionty.tissue.id", index=True
+    )
+    tissue = Relationship()
+    cell_type_id: Optional[str] = Field(
+        default=None, foreign_key="bionty.cell_type.id", index=True
+    )
+    cell_type = Relationship()
+    disease_id: Optional[str] = Field(
+        default=None, foreign_key="bionty.disease.id", index=True
+    )
+    disease = Relationship()
+    dobjects: DObject = Relationship(
+        back_populates="biosamples",
+        sa_relationship_kwargs=dict(secondary=DObjectBiosample.__table__),
+    )
+
+
+DObject.biosamples = relationship(
+    Biosample, back_populates="dobjects", secondary=DObjectBiosample.__table__
+)
+DObject.__sqlmodel_relationships__["biosamples"] = None
+
+add_relationship_keys(Biosample)
+
+
+class Techsample(TechsampleBase, table=True):  # type: ignore
+    biosamples: Biosample = Relationship(
+        back_populates="techsamples",
+        sa_relationship_kwargs=dict(secondary=BiosampleTechsample.__table__),
+    )
+
+
+Biosample.techsamples = relationship(
+    Techsample, back_populates="biosamples", secondary=BiosampleTechsample.__table__
+)
+Biosample.__sqlmodel_relationships__["techsamples"] = None
+
+
+class Readout(ReadoutBase, table=True):  # type: ignore
+    """Biological samples that are registered in experiments."""
+
+    dobjects: DObject = Relationship(
+        back_populates="readouts",
+        sa_relationship_kwargs=dict(secondary=DObjectReadout.__table__),
+    )
+
+
+DObject.readouts = relationship(
+    Readout,
+    back_populates="dobjects",
+    secondary=DObjectReadout.__table__,
+)
+DObject.__sqlmodel_relationships__["readouts"] = None
--- lnschema_wetlab/_core.py
+++ lnschema_wetlab/_core.py
@@ -2,63 +2,55 @@ from datetime import datetime as datetime
 from typing import Optional
 
 from lndb_setup import settings
-from lnschema_bionty import CellType, Disease, Species, Tissue
+from lnschema_bionty import Species
 from lnschema_core import DObject, Project  # noqa
 from lnschema_core._timestamps import CreatedAt, UpdatedAt
 from lnschema_core._users import CreatedBy
 from lnschema_core.dev.sqlmodel import schema_sqlmodel
 from sqlalchemy.orm import declared_attr, relationship
-from sqlmodel import Field, Relationship
+from sqlmodel import Field
 
 from . import _name as schema_name
-from ._link import (  # noqa
-    BiosampleTechsample,
-    DObjectBiosample,
-    DObjectExperiment,
-    DObjectReadout,
-    ProjectExperiment,
-)
 from .dev import id as idg
 
 SQLModel, prefix, schema_arg = schema_sqlmodel(schema_name)
 
 
-def add_relationship_keys(table: SQLModel):  # type: ignore
-    """add all relationship keys to __sqlmodel_relationships__."""
-    for i in getattr(table, "__mapper__").relationships:
-        if i not in getattr(table, "__sqlmodel_relationships__"):
-            getattr(table, "__sqlmodel_relationships__")[i.key] = None
+class ExperimentBase(SQLModel):  # type: ignore
+    """Experiments."""
 
-
-class Treatment(SQLModel, table=True):  # type: ignore
-    """Treatment."""
-
-    id: str = Field(default_factory=idg.treatment, primary_key=True)
-    external_id: str = Field(default=None, unique=True, index=True)
+    id: str = Field(default_factory=idg.experiment, primary_key=True)
     name: str = Field(default=None, index=True)
+    external_id: str = Field(default=None, unique=True)
+    date: datetime = Field(default=None, index=True)
+    """Date on which the experiment is performed."""
     created_by: str = CreatedBy
     created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
+
+
+class ExperimentTypeBase(SQLModel):  # type: ignore
+    """Experiment types.
+
+    Useful for bucketing experiments.
+    """
+
+    id: str = Field(default_factory=idg.experiment_type, primary_key=True)
+    name: str = Field(default=None, index=True)
+    efo_id: str = Field(default=None, unique=True)
+    created_by: str = CreatedBy
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
 
 
 class BiosampleBase(SQLModel):  # type: ignore
+    """Biosamples: the fundamental observational unit."""
+
     id: str = Field(default_factory=idg.biosample, primary_key=True)
-    external_id: Optional[str] = Field(default=None, index=True, unique=True)
     name: Optional[str] = Field(default=None, index=True)
-    batch: Optional[str] = None
+    external_id: Optional[str] = Field(default=None, index=True, unique=True)
     species_id: Optional[str] = Field(
         default=None, foreign_key="bionty.species.id", index=True
-    )
-    tissue_id: Optional[str] = Field(
-        default=None, foreign_key="bionty.tissue.id", index=True
-    )
-    cell_type_id: Optional[str] = Field(
-        default=None, foreign_key="bionty.cell_type.id", index=True
-    )
-    disease_id: Optional[str] = Field(
-        default=None, foreign_key="bionty.disease.id", index=True
-    )
-    treatment_id: Optional[str] = Field(
-        default=None, foreign_key="wetlab.treatment.id", index=True
     )
     created_by: str = CreatedBy
     created_at: datetime = CreatedAt
@@ -69,29 +61,13 @@ class BiosampleBase(SQLModel):  # type: ignore
     def species(self) -> Optional[Species]:
         return relationship("Species")
 
-    @declared_attr
-    def tissue(self) -> Optional[Tissue]:
-        return relationship("Tissue")
-
-    @declared_attr
-    def cell_type(self) -> Optional[CellType]:
-        return relationship("CellType")
-
-    @declared_attr
-    def disease(self) -> Optional[Disease]:
-        return relationship("Disease")
-
-    @declared_attr
-    def treatment(self) -> Optional[Treatment]:
-        return relationship("Treatment")
-
 
 class TechsampleBase(SQLModel):  # type: ignore
     """Tech samples that are generated due to instrument units."""
 
     id: str = Field(default_factory=idg.techsample, primary_key=True)
-    external_id: Optional[str] = Field(default=None, index=True, unique=True)
     name: Optional[str] = Field(default=None, index=True)
+    external_id: Optional[str] = Field(default=None, index=True, unique=True)
     batch: Optional[str] = None
     filepath_r1: Optional[str] = None
     filepath_r2: Optional[str] = None
@@ -100,104 +76,27 @@ class TechsampleBase(SQLModel):  # type: ignore
     updated_at: Optional[datetime] = UpdatedAt
 
 
-if "wetlab" in settings.instance.schema:
-
-    class Biosample(BiosampleBase, table=True):  # type: ignore
-        """Biological samples that are registered in experiments."""
-
-        dobjects: DObject = Relationship(
-            back_populates="biosamples",
-            sa_relationship_kwargs=dict(secondary=DObjectBiosample.__table__),
-        )
-
-    DObject.biosamples = relationship(
-        Biosample, back_populates="dobjects", secondary=DObjectBiosample.__table__
-    )
-    DObject.__sqlmodel_relationships__["biosamples"] = None
-
-    add_relationship_keys(Biosample)
-
-    class Techsample(TechsampleBase, table=True):  # type: ignore
-        biosamples: Biosample = Relationship(
-            back_populates="techsamples",
-            sa_relationship_kwargs=dict(secondary=BiosampleTechsample.__table__),
-        )
-
-    Biosample.techsamples = relationship(
-        Techsample, back_populates="biosamples", secondary=BiosampleTechsample.__table__
-    )
-    Biosample.__sqlmodel_relationships__["techsamples"] = None
-
-else:
-    Biosample = None  # type: ignore
-    Techsample = None  # type: ignore
-
-
-class Readout(SQLModel, table=True):  # type: ignore
-    """Readout of experiments."""
+class ReadoutBase(SQLModel):  # type: ignore
+    """Readouts."""
 
     id: str = Field(default_factory=idg.readout, primary_key=True)
-    efo_id: str = Field(default=None, unique=True, index=True)
+    efo_id: Optional[str] = Field(default=None, unique=True, index=True)
     name: Optional[str] = None
     molecule: Optional[str] = None
     instrument: Optional[str] = None
     measurement: Optional[str] = None
     created_by: str = CreatedBy
     created_at: datetime = CreatedAt
-    dobjects: DObject = Relationship(
-        back_populates="readouts",
-        sa_relationship_kwargs=dict(secondary=DObjectReadout.__table__),
-    )
+    updated_at: Optional[datetime] = UpdatedAt
 
 
-DObject.readouts = relationship(
-    Readout,
-    back_populates="dobjects",
-    secondary=DObjectReadout.__table__,
-)
-DObject.__sqlmodel_relationships__["readouts"] = None
+# the following is just for testing purposes
+if "wetlab" in settings.instance.schema:
+    from ._actual import Biosample, Experiment, ExperimentType, Readout, Techsample
 
-
-class Experiment(SQLModel, table=True):  # type: ignore
-    """Experiments."""
-
-    id: str = Field(default_factory=idg.experiment, primary_key=True)
-    external_id: str = Field(default=None, unique=True)
-    name: str = Field(default=None, index=True)
-    date: datetime = Field(default=None, index=True)
-    experiment_type_id: str = Field(
-        default=None, foreign_key="wetlab.experiment_type.id", index=True
-    )
-    created_by: str = CreatedBy
-    created_at: datetime = CreatedAt
-    dobjects: DObject = Relationship(
-        back_populates="experiments",
-        sa_relationship_kwargs=dict(secondary=DObjectExperiment.__table__),
-    )
-    # projects: Project = Relationship(
-    #     back_populates="experiments",
-    #     sa_relationship_kwargs=dict(secondary=ProjectExperiment.__table__),
-    # )
-
-
-DObject.experiments = relationship(
-    Experiment, back_populates="dobjects", secondary=DObjectExperiment.__table__
-)
-DObject.__sqlmodel_relationships__["experiments"] = None
-
-# Project.experiments = relationship(
-#     Experiment, back_populates="projects", secondary=ProjectExperiment.__table__
-# )
-# Project.__sqlmodel_relationships__["experiments"] = None
-
-
-class ExperimentType(SQLModel, table=True):  # type: ignore
-    """Experiment types."""
-
-    __tablename__ = f"{prefix}experiment_type"
-
-    id: str = Field(default_factory=idg.experiment_type, primary_key=True)
-    name: str = Field(default=None, index=True)
-    efo_id: str = Field(default=None, unique=True)
-    created_by: str = CreatedBy
-    created_at: datetime = CreatedAt
+else:
+    Experiment = None  # type: ignore
+    ExperimentType = None  # type: ignore
+    Biosample = None  # type: ignore
+    Techsample = None  # type: ignore
+    Readout = None  # type: ignore
